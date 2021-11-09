3a92619709598af5504a0aeadd4bd5a9d1bc8110
Unknown <84217207+seaweedq12@users.noreply.github.com>
2021-11-09 17:06:05 +1100

    final edit

--- client/src/components/Myinfobar/index.js
+++ client/src/components/Myinfobar/index.js
@@ -9,7 +9,7 @@ function Myinfobar({ handlePageChange }) {
           href="#myinfo"
           onClick={() => handlePageChange('Myinfo')}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className='nav-link'
+          className='nav-link subnav'
         >
           About
         </a>
@@ -20,7 +20,7 @@ function Myinfobar({ handlePageChange }) {
           onClick={() => handlePageChange('Order')}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className= 'nav-link'
+          className= 'nav-link subnav'
         >
           Order
         </a>
